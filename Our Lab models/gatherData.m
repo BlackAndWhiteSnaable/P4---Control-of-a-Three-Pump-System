67ac35e3404ab57164779891b1ff2301fa5f27ca
Daniel <BlackAndWhiteSnaable@gmail.com>
2018-03-21 11:41:10 +0100

    auto update the run_# folder

--- Our Lab models/gatherData.m
+++ Our Lab models/gatherData.m
@@ -4,9 +4,8 @@ clc;clear
 % 'P[1/2/3]_[CV01%]'
 % ED4 is only working on Pump2
 
-%which run is this? increment before running
-run_num = 2;
-
+%gets automatically incremented if folder already exists
+run_num = 0;
 
 %valve settling time < 5 seconds
 CV01t = 5;
@@ -20,8 +19,9 @@ simstop = numel(WPtime)*10+CV01t;
 targetfile = ['E:\P2.dat'];
 hostfolder = ['dataFromTarget\run_' int2str(run_num) '\'];
 %make sure the folder exists
-if (exist(hostfolder, 'dir') ~= 7)
-    mkdir(hostfolder)
+while (exist(hostfolder, 'dir') == 7)
+    run_num = run_num + 1;
+    hostfolder = ['dataFromTarget\run_' int2str(run_num) '\'];
 end
 
 %% select valve opening(backpressure) by for loop
