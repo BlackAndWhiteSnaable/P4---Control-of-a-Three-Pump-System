0941bc92423a59766d27fedcd0c6d51a545e6092
Daniel <BlackAndWhiteSnaable@gmail.com>
2018-05-08 15:44:25 +0200

    new figures

--- Our Lab models/SaveAndCropSLX.m
+++ Our Lab models/SaveAndCropSLX.m
@@ -2,7 +2,7 @@
 clc;clear
 % OPEN THE SIMULINK FILE TO SAVE
 % name of the outputfile
-out = 'C';
+out = 'CLblock';
 
 % chapter for this to appear in
 chapter = '04ExperimentsAndLabWork';
@@ -10,6 +10,6 @@ chapter = '04ExperimentsAndLabWork';
 
 %% DON'T CHANGE ANYTHING BELOW
 print('-s','-dpdf',out)
-crop = ['pdfcrop ' out ' ../Report/figures/' chapter '/' out '_cropped' '.pdf'];
+crop = ['pdfcrop ' out ' ../Report/figures/' chapter '/' out '.pdf'];
 system(crop);
 delete([out '.pdf']);
