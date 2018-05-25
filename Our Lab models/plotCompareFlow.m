3db6cef0b4682331dd503e81b37d95dbaf61bf12
Daniel <BlackAndWhiteSnaable@gmail.com>
2018-05-25 13:50:21 +0200

    fixed some scripts, figures now all have the same size
    also updated some figures
    don't scale figures, width them
    always write .eps so the changes get updated automatically

--- Our Lab models/plotCompareFlow.m
+++ Our Lab models/plotCompareFlow.m
@@ -5,12 +5,21 @@ if ~exist('pump2_meas', 'var')
 end
 abs_max = 0; abs_min = 50; x_blank = 0;
 
+time = pump2_meas(1).data(:,end);
+
 %% PLOTTING
 % plot all flow curves
-figure('Name',['Flow Comparison run',num2str(run_num)],'NumberTitle','off')
+figure('Name',['Flow Comparison run',num2str(run_num)],'NumberTitle','off','Position',[5 50 800 400])
 for j = 1:numel(pump2_meas)
-    name = [num2str(j*10) '% CV01'];
-    plot(pump2_meas(j).data(:,end),pump2_meas(j).data(:,8),'DisplayName',name)
+    name = ['$CV_1 =' num2str(j*10) '\%$'];
+    if j == 1
+        style = '-v';
+    elseif j == numel(pump2_meas)
+        style = '-^';
+    else
+        style = '-';
+    end
+    plot(time,pump2_meas(j).data(:,8),style,'MarkerIndices',1500:1500:length(time),'DisplayName',name,'markers',12)
     %set the axis to go from absolute minimum-1% to absolute maximum+1%
     if max(pump2_meas(j).data(:,8)) > abs_max
         abs_max = max(pump2_meas(j).data(:,8));
@@ -22,9 +31,11 @@ for j = 1:numel(pump2_meas)
     axis([0 pump2_meas(1).data(end,end) abs_min-x_blank abs_max+x_blank])
     hold on; grid on
 end
-xlabel('Time [s]', 'Interpreter', 'latex');
-ylabel('Flow [$m^3/h$]', 'Interpreter', 'latex');
-legend('show','Location','northwest');
+xlabel('Time [s]', 'Interpreter', 'latex','fontsize',18);
+ylabel('Q [$\frac{m^3}{h}$]', 'Interpreter', 'latex','fontsize',18);
+leg = legend('show','Location','northwest');
+set(leg,'Interpreter','latex');
+set(leg,'FontSize',12);
 
 %% cleanup
-clearvars abs_max abs_min j name x_blank
+clearvars abs_max abs_min j name x_blank time style leg
--- Our Lab models/plotComparePower.m
+++ Our Lab models/plotComparePower.m
@@ -4,13 +4,22 @@ if ~exist('pump2_meas', 'var')
     ImportData2Workspace;
 end
 abs_max = 0; abs_min = 5; x_blank = 0;
+
+time = pump2_meas(1).data(:,end);
     
 %% PLOTTING
 % plot all power curves
-figure('Name',['Power Comparison run',num2str(run_num)],'NumberTitle','off')
+figure('Name',['Power Comparison run',num2str(run_num)],'NumberTitle','off','Position',[200 800 800 400])
 for j = 1:numel(pump2_meas)
-    name = [num2str(j*10) '% CV01'];
-    plot(pump2_meas(j).data(:,end),pump2_meas(j).data(:,7),'DisplayName',name)
+    name = ['$CV_1 =' num2str(j*10) '\%$'];
+    if j == 1
+        style = ':v';
+    elseif j == numel(pump2_meas)
+        style = ':^';
+    else
+        style = '-';
+    end
+    plot(time,pump2_meas(j).data(:,7),style,'MarkerIndices',1500:1500:length(time),'DisplayName',name,'markers',12)
     %set the axis to go from absolute minimum-1% to absolute maximum+1%
     if max(pump2_meas(j).data(:,7)) > abs_max
         abs_max = max(pump2_meas(j).data(:,7));
@@ -22,9 +31,11 @@ for j = 1:numel(pump2_meas)
     axis([0 pump2_meas(1).data(end,end) abs_min-x_blank abs_max+x_blank])
     hold on; grid on
 end
-xlabel('Time[s]', 'Interpreter', 'latex');
-ylabel('Power[watt]', 'Interpreter', 'latex');
-legend('show','Location','northwest');
+xlabel('Time[$s$]', 'Interpreter', 'latex','fontsize',18);
+ylabel('Power[$W$]', 'Interpreter', 'latex','fontsize',18);
+leg = legend('show','Location','northwest');
+set(leg,'Interpreter','latex');
+set(leg,'FontSize',12);
 
 %% cleanup
 clearvars abs_max abs_min j name x_blank
