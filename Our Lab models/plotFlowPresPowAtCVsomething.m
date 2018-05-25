459fd2907168772bdb388fc26c24ff9efe7e65c3
Daniel <BlackAndWhiteSnaable@gmail.com>
2018-05-25 13:11:01 +0200

    fixed matlab script to be compatible with colorblindness (hopefully)
    script, .fig, .eps and .pdf included in commit
    also included the figure in latex

--- Our Lab models/plotFlowPresPowAtCVsomething.m
+++ Our Lab models/plotFlowPresPowAtCVsomething.m
@@ -1,6 +1,6 @@
 %% make sure all variables exist
 % chose run_num to import
-run_num = 10;
+run_num = 32;
 
 if ~exist('pump2_meas','var')
     ImportData2Workspace
@@ -9,42 +9,54 @@ end
 % plot all curves at cv=70
 j = 6;
 
-%% offset and gain all relevant curves
+%% label all variables
+% time
+time = pump2_meas(j).data(:,end);
+
 % flow
-offset_q = ones(numel(pump2_meas(j).data(:,8)),1)*1.8810;
 flow = pump2_meas(j).data(:,8);
-flow = flow-offset_q;
-flow = flow * 2.6691;
 
 % head
-offset_h = ones(numel(pump2_meas(j).data(:,8)),1)*2.8343;
 head = pump2_meas(j).data(:,6);
-head = head - offset_h;
-head = head * 5;     %we don't know the gain
+
+% power
+power = pump2_meas(j).data(:,7);
+
+% input speed
+speed = pump2_meas(j).data(:,1);
 
 %% create figure
-figure('Name',['Flow,Pressure,Power comparison; CV01=',num2str(j)],'NumberTitle','off')
+figure('Name',['Flow,Pressure,Power comparison; CV01=',num2str(j)],'NumberTitle','off','Position',[5 50 800 400])
 title(['Stair Response with $CV_1 = ' num2str(j*10) '\%$'],'Interpreter','latex')
 hold on; grid on
 
 %% plot Flow
+fstyle = '-v';
 name_flow = 'Q [$\frac{m^3}{s}$]';
-plot(pump2_meas(j).data(:,end),flow,'DisplayName',name_flow)
+plot(time,flow,fstyle,'MarkerIndices',1500:1500:length(time),'DisplayName',name_flow)
 
 %% plot Pressure
-name_pres = 'H [$m$]';
-plot(pump2_meas(j).data(:,end),head,'DisplayName',name_pres)
+hstyle = '-.x';
+name_pres = 'H [$bar$]';
+plot(time,head,hstyle,'MarkerIndices',1500:1500:length(time),'DisplayName',name_pres)
 
 %% plot Power
-name_powr = 'P [$W$]';
-plot(pump2_meas(j).data(:,end),pump2_meas(j).data(:,7),'DisplayName',name_powr)
+pstyle = '-o';
+name_powr = 'P [$kW$]';
+plot(time,power/1000,pstyle,'MarkerIndices',1500:1500:length(time),'DisplayName',name_powr)
 
 %% plot input speed
-name = '$\omega P_{2in}$ [$\frac{\%}{10}$]';
-plot(pump2_meas(j).data(:,end),pump2_meas(j).data(:,1)/10,'DisplayName',name)
+sstyle = '-^';
+name = '$\omega P_{2in}$ [$10\%$]';
+plot(time,speed/10,sstyle,'MarkerIndices',1500:1500:length(time),'DisplayName',name)
 
+%% set axis
+axis([0 170 -0.5 11])
+
+%% label axis
+xlabel('Time [s]','fontsize',18,'Interpreter','latex')
 
 %% build legend
-leg = legend('Location','southeast');
+leg = legend('Location','northwest');
 set(leg,'Interpreter','latex');
 set(leg,'FontSize',12);
