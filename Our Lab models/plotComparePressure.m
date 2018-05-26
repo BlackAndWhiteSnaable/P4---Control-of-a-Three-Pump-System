0b2737c4205372b0bb99d6075645f8f1ddee81b0
Daniel <BlackAndWhiteSnaable@gmail.com>
2018-05-26 19:21:15 +0200

    ML script to create readable Pressure comp

--- Our Lab models/plotComparePressure.m
+++ Our Lab models/plotComparePressure.m
@@ -1,21 +1,25 @@
 %% SETUP
 clc;
-%if run_num exists, all other necessary ones should exist
-if exist('run_num', 'var')
-    if ~exist('pump2_meas', 'var')
-        ImportData2Workspace
-    end
-else
+if ~exist('pump2_meas', 'var')
     ImportData2Workspace;
 end
 abs_max = 0; abs_min = 50; x_blank = 0;
+
+time = pump2_meas(1).data(:,end);
     
 %% PLOTTING
 % plot all pressure curves
-figure('Name',['Pressure Comparison run',num2str(run_num)],'NumberTitle','off')
+figure('Name',['Pressure Comparison run',num2str(run_num)],'NumberTitle','off','Position',[5 50 800 400])
 for j = 1:numel(pump2_meas)
-    name = [num2str(j*10) '% CV01'];
-    plot(pump2_meas(j).data(:,end),pump2_meas(j).data(:,6),'DisplayName',name)
+    name = ['$CV_1 =' num2str(j*10) '\%$'];
+    if j == 1
+        style = '-v';
+    elseif j == numel(pump2_meas)
+        style = '-^';
+    else
+        style = '-';
+    end
+    plot(time,pump2_meas(j).data(:,6),style,'MarkerIndices',1500:1500:length(time),'DisplayName',name,'markers',12)
     %set the axis to go from absolute minimum-1% to absolute maximum+1%
     if max(pump2_meas(j).data(:,6)) > abs_max
         abs_max = max(pump2_meas(j).data(:,6));
@@ -27,8 +31,11 @@ for j = 1:numel(pump2_meas)
     axis([0 pump2_meas(1).data(end,end) abs_min-x_blank abs_max+x_blank])
     hold on; grid on
 end
-title('Pressure Comparison');
-xlabel('Time [s]', 'Interpreter', 'latex');
-ylabel('Pressure [bar]', 'Interpreter', 'latex');
-legend('show','Location','northwest');
-clearvars abs_max abs_min j name x_blank
+xlabel('Time [s]', 'Interpreter', 'latex','fontsize',18);
+ylabel('H [$\frac{m^3}{h}$]', 'Interpreter', 'latex','fontsize',18);
+leg = legend('show','Location','northwest');
+set(leg,'Interpreter','latex');
+set(leg,'FontSize',12);
+
+%% cleanup
+clearvars abs_max abs_min j name x_blank time style leg
